4e5947dacbb2faa3f909c143ed0edd0510dd31dc
Matheus Nobre <matheusnobre56@gmail.com>
2022-04-18 22:22:51 -0300

    convert memory database to sqlite

--- models/hotel.py
+++ models/hotel.py
@@ -8,13 +8,15 @@ class HotelModel(database.Model):
     daily = database.Column(database.Float(precision = 2))
     city = database.Column(database.String(40))
 
+
     def __init__(self, id, name, grade, daily, city):
         self.id = id
         self.name = name
         self.grade = grade
         self.daily = daily
         self.city = city
-    
+
+
     def json(self):
         return {
             'id': self.id,
@@ -23,3 +25,29 @@ class HotelModel(database.Model):
             'daily': self.daily,
             'city': self.city
         }
+
+
+    @classmethod
+    def find(cls, id):
+        # SELECT * FROM hotels WHERE id = :id
+        hotel = cls.query.filter_by(id=id).first()
+        if hotel:
+            return hotel
+        return None
+
+
+    def save(self):
+        database.session.add(self)
+        database.session.commit()
+
+
+    def update(self, name, grade, daily, city):
+        self.name = name
+        self.grade = grade
+        self.daily = daily
+        self.city = city
+
+
+    def delete(self):
+        database.session.delete(self)
+        database.session.commit()
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,81 +1,66 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hotels = [
-    {
-        'id': 'alpha',
-        'name': 'Alpha Hotel',
-        'grade': 4.3,
-        'daily': 540.90,
-        'city': 'São Paulo'
-    },
-    {
-        'id': 'bravo',
-        'name': 'Bravo Hotel',
-        'grade': 3.9,
-        'daily': 400.55,
-        'city': 'Rio de Janeiro'
-    },
-    {
-        'id': 'charlie',
-        'name': 'Charlie Hotel',
-        'grade': 4.9,
-        'daily': 550.00,
-        'city': 'Rio de Janeiro'
-    }
-]
-
 class Hotels(Resource):
     def get(self):
-        return {'success': True, 'data': hotels}
+        return {'success': True, 'data': [hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
     form = reqparse.RequestParser()
-    form.add_argument('name')
-    form.add_argument('grade')
-    form.add_argument('daily')
-    form.add_argument('city')
+    form.add_argument('name', type=str, required=True, help="Field 'name' is required.")
+    form.add_argument('grade', type=float, required=True, help="Field 'grade' is required.")
+    form.add_argument('daily', type=float, required=True, help="Field 'daily' is required.")
+    form.add_argument('city', type=str, required=True, help="Field 'city' is required.")
 
-    def find(hotel_id):
-        return next((hotel for hotel in hotels if hotel['id'] == hotel_id), None)
 
     def get(self, id):
-        hotel = Hotel.find(id)
+        hotel = HotelModel.find(id)
         if hotel:
-            return {'success': True, 'data': hotel}
+            return {'success': True, 'data': hotel.json()}
         return {'success': False, 'message': 'Hotel not found'}, 404
-    
+
+
     def post(self, id):
-        hotel = Hotel.find(id)
-        if hotel:
+        if HotelModel.find(id):
             return {'success': False, 'data': 'Hotel already exists'}, 409
 
         data = Hotel.form.parse_args()
-        new_hotel = HotelModel(id, **data).json()
+        hotel = HotelModel(id, **data)
 
-        hotels.append(new_hotel)
+        try:
+            hotel.save()
+            return {'success': True, 'data': hotel.json()}, 201
+        except:
+            return {'success': False, 'message': 'An internal error ocurred trying to save hotel'}, 500
 
-        return {'success': True, 'data': new_hotel}, 201
-    
+
     def put(self, id):
         data = Hotel.form.parse_args()
-        new_hotel = HotelModel(id, **data).json()
 
-        hotel = Hotel.find(id)
+        hotel = HotelModel.find(id)
         if hotel:
-            hotel.update(new_hotel)
-            return {'success': True, 'data': hotel}
+            hotel.update(**data)
+            try:
+                hotel.save()
+                return {'success': True, 'data': hotel.json()}
+            except:
+                return {'success': False, 'message': 'An internal error ocurred trying to save hotel'}, 500
 
-        hotels.append(new_hotel)
+        new_hotel = HotelModel(id, **data)
+        try:
+            new_hotel.save()
+            return {'success': True, 'data': new_hotel.json()}, 201
+        except:
+            return {'success': False, 'message': 'An internal error ocurred trying to save hotel'}, 500
 
-        return {'success': True, 'data': new_hotel}, 201
 
     def delete(self, id):
-        hotel = Hotel.find(id)
+        hotel = HotelModel.find(id)
         if hotel:
-            hotels.remove(hotel)
-            return {'success': True}
+            try:
+                hotel.delete()
+                return {'success': True}
+            except:
+                return {'success': False, 'message': 'An internal error ocurred trying to delete hotel'}, 500
 
         return {'success': False, 'message': 'Hotel not found'}, 404
-
-         
