2679850e118b7c35b61eb353ff8f825296438a30
Matheus Nobre <matheusnobre56@gmail.com>
2022-04-18 21:33:01 -0300

    sql_alchemy

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask_restful import Api
 from resources.hotel import Hotels, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_first_request
+def connect():
+    database.create_all()
 
 api.add_resource(Hotels, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import database
+    database.init_app(app)
     app.run(debug=True)
--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,13 @@
-class HotelModel:
+from sql_alchemy import database
+
+class HotelModel(database.Model):
+    __tablename__ = 'hotels'
+    id = database.Column(database.String, primary_key = True)
+    name = database.Column(database.String(80))
+    grade = database.Column(database.Float(precision = 1))
+    daily = database.Column(database.Float(precision = 2))
+    city = database.Column(database.String(40))
+
     def __init__(self, id, name, grade, daily, city):
         self.id = id
         self.name = name
