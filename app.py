35e174463799eee2332cac0215e6ec44357f1238
Matheus Nobre <matheusnobre56@gmail.com>
2022-04-23 12:39:19 -0300

    prepare to deploy

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ from resources.site import Sites, Site
 from resources.user import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
 from blacklist import BLACKLIST
+from sql_alchemy import database
 
 app = Flask(__name__)
+database.init_app(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pyvago.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY1MDU1Mjk1NywiaWF0IjoxNjUwNTUyOTU3fQ.OBL4_3RCM-zICx7BSTl1uiQtah_hMEUYYwZjiFKNQ6M"
 app.config['JWT_BLACKLIST_ENABLED'] = True
 api = Api(app)
 jwt = JWTManager(app)
+
+@app.route('/')
+def index():
+    return "<h1>Bem vindo ao PyVago!</h1>"
 
 @app.before_first_request
 def connect():
@@ -34,8 +41,3 @@ api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
 api.add_resource(Sites, '/sites')
 api.add_resource(Site, '/sites/<int:id>')
-
-if __name__ == '__main__':
-    from sql_alchemy import database
-    database.init_app(app)
-    app.run(debug=True)
